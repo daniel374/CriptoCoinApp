9be15b28c53c31375d46c3eb7db45f87b9ec9972
DDDanieLLL Rodriguez Layton <dannilayton374@gmail.com>
2021-11-03 02:51:01 -0500

    Se consume la Api CoinMarketCap API para traer la informaciónd e las cripto monedas y presentarlas en pantalla.

--- flutter_cripto_coins/lib/money/ui/screens/menu_money_home/show_crypto_moneys.dart
+++ flutter_cripto_coins/lib/money/ui/screens/menu_money_home/show_crypto_moneys.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_cripto_coins/widgets/gradient_back.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:http/http.dart' as http;
 
 const List<StaggeredTile> _staggeredTiles = <StaggeredTile>[
   StaggeredTile.count(4, 1),
@@ -9,17 +13,66 @@ const List<StaggeredTile> _staggeredTiles = <StaggeredTile>[
   StaggeredTile.count(4, 1),
 ];
 
-const List<Widget> _tiles = <Widget>[
-  _ShowCryptoMoneys(Colors.black, Icons.radio, "AUDNZD", '03.27.27', "105361.00000", "104189.00000", "36", "1.05222", "1.05258", Colors.blue),
-  _ShowCryptoMoneys(Colors.black, Icons.radio, "AUDCHF", '12.00.00', "--", "--", "0", "0.00000", "0.00000", Colors.blue),
-  _ShowCryptoMoneys(Colors.black, Icons.radio, "AUDCAD", '03.27.37', "92268.00000", "91717.00000", "24", "0.91725", "0.91749", Colors.blue),
-  _ShowCryptoMoneys(Colors.black, Icons.radio, "AUDJPY", '03.27.28', "80.234", "79.573", "28", "79.632", "79.660", Colors.red),
-  _ShowCryptoMoneys(Colors.black, Icons.radio, "CHFJPY", '03.27.36', "120.098", "119.567", "21", "120.063", "120.084", Colors.blue),
-];
+// const List<_ShowCryptoMoneys> _tiles = [
+//   _ShowCryptoMoneys(Colors.black, Icons.radio, "AUDNZD", '03.27.27', "105361.00000", "104189.00000", "36", "1.05222", "1.05258", Colors.blue),
+//   _ShowCryptoMoneys(Colors.black, Icons.radio, "AUDCHF", '12.00.00', "--", "--", "0", "0.00000", "0.00000", Colors.blue),
+//   _ShowCryptoMoneys(Colors.black, Icons.radio, "AUDCAD", '03.27.37', "92268.00000", "91717.00000", "24", "0.91725", "0.91749", Colors.blue),
+//   _ShowCryptoMoneys(Colors.black, Icons.radio, "AUDJPY", '03.27.28', "80.234", "79.573", "28", "79.632", "79.660", Colors.red),
+//   _ShowCryptoMoneys(Colors.black, Icons.radio, "CHFJPY", '03.27.36', "120.098", "119.567", "21", "120.063", "120.084", Colors.blue),
+// ];
 
 
-class ShowCryptoMoneys extends StatelessWidget {
+class ShowCryptoMoneys extends StatefulWidget {
 
+  @override
+  State<ShowCryptoMoneys> createState() => _ShowCryptoMoneysState();
+}
+
+class _ShowCryptoMoneysState extends State<ShowCryptoMoneys> {
+
+  Future<List<_ShowCryptoMoneys>> _listCryptoMOneys;
+
+  Future<List<_ShowCryptoMoneys>> _getListCryptoMOneys() async {
+    final response = await http.get(Uri.parse("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"),
+        headers: {"X-CMC_PRO_API_KEY":"9dc108f9-84a3-404f-8fa1-9c18d13672d3"}
+        );
+
+    List<_ShowCryptoMoneys> showCrypto = [];
+
+    if (response.statusCode == 200) {
+      String body = utf8.decode(response.bodyBytes);
+
+      final jsonData = jsonDecode(body);
+      /*print(jsonData["data"][0]["name"]);*/
+      for(var item in jsonData["data"]){
+        showCrypto.add(
+          _ShowCryptoMoneys(Colors.black,
+              Icons.radio,
+              item["name"],
+              '03.27.27',
+              "105361.00000",
+              "104189.00000",
+              "36",
+              "1.05222",
+              "1.05258",
+              Colors.blue)
+        );
+      }
+
+      print(showCrypto);
+      return showCrypto;
+
+    } else {
+      throw Exception("Failed conection");
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _listCryptoMOneys = _getListCryptoMOneys();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,22 +81,87 @@ class ShowCryptoMoneys extends StatelessWidget {
         appBar: AppBar(
           title: const Text('De Cotizaciones'),
         ),
-        body: Padding(
-            padding: const EdgeInsets.only(top: 12),
-            child: StaggeredGridView.count(
-              crossAxisCount: 4,
-              staggeredTiles: _staggeredTiles,
-              mainAxisSpacing: 4,
-              crossAxisSpacing: 4,
-              padding: const EdgeInsets.all(4),
-              children: _tiles,
-            )));
+        body: FutureBuilder(
+          future: _listCryptoMOneys,
+          builder: (context, snapshot) {
+
+            if (snapshot.hasData) {
+              print(snapshot.data);
+              return Stack(
+                children: [
+                  GradientBack("", null),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12),
+                    child: StaggeredGridView.count(
+                    crossAxisCount: 4,
+                    staggeredTiles: _staggeredTiles,
+                    mainAxisSpacing: 4,
+                    crossAxisSpacing: 4,
+                    padding: const EdgeInsets.all(4),
+                    children: _listCryptoMOneysData(snapshot.data),//_listCryptoMOneysData(snapshot.data.documents.map),//_tiles
+                    )
+                  ),
+                ],
+              );
+
+            } else if (snapshot.hasError) {
+              print(snapshot.hasError);
+              return Text("Error");
+            }
+
+            return Center(
+            child: CircularProgressIndicator(),
+            );
+          },
+        ),
+    );
+  }
+
+  List<Widget> _listCryptoMOneysData(List<_ShowCryptoMoneys> data){
+    List<Widget> moneys = [];
+
+    for(var money in data){
+      moneys.add(_CriptoMoneyCard(Colors.black, Icons.radio, money.cripto_money, '03.27.27', "105361.00000", "104189.00000", "36", "1.05222", "1.05258", Colors.blue));
+    }
+
+    return moneys.toList();
+
   }
 }
 
-class _ShowCryptoMoneys extends StatelessWidget {
+class _ShowCryptoMoneys {
 
-  const _ShowCryptoMoneys(
+  Color backgroundColor;
+  IconData iconData;
+  String cripto_money;
+  String money_num;
+  String hight_value;
+  String low_value;
+  String differential_value;
+  String one_value;
+  String two_value;
+  Color color_values;
+
+  _ShowCryptoMoneys(backgroundColor, iconData, cripto_money,
+      money_num, hight_value,
+      low_value, differential_value,
+      one_value, two_value, color_values) {
+    this.backgroundColor = backgroundColor;
+    this.iconData = iconData;
+    this.cripto_money = cripto_money;
+    this.money_num = money_num;
+    this.hight_value = hight_value;
+    this.low_value = low_value;
+    this.differential_value = differential_value;
+    this.one_value = one_value;
+    this.two_value = two_value;
+    this.color_values = color_values;
+  }
+}
+
+class _CriptoMoneyCard extends StatelessWidget {
+
+  const _CriptoMoneyCard(
       this.backgroundColor, this.iconData, this.cripto_money, this.money_num,
       this.hight_value, this.low_value, this.differential_value,
       this.one_value, this.two_value, this.color_values
@@ -59,7 +177,6 @@ class _ShowCryptoMoneys extends StatelessWidget {
   final String one_value;
   final String two_value;
   final Color color_values;
-
 
   @override
   Widget build(BuildContext context) {
